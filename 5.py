b9db63bb649b968262d4473d67ae72db0a35d3fa
SamuelHenriquess <samuel.henriquess21@gmail.com>
2022-10-05 16:02:53 -0300

    5 WIP

--- 5.py
+++ 5.py
@@ -4,11 +4,11 @@ Faça com que no programa 2 o usuário pergunte um dia da semana e seu programa 
 semana = {}
 
 aulas = [
-    ['AP41', 'PR43'],
-    ['AE41', 'EM41'],
-    ['LP41', 'PR43'],
-    ['RL44', 'ET44', 'EF44'],
-    ['DS44', 'PJ42']
+    {'AP41': "14:10", 'PR43': "16:10"},
+    {'AE41': "14:10", 'EM41': "16:10"},
+    {'LP41': "14:10", 'PR43': "16:10"},
+    {'RL44':"14:10" , 'ET44': "15:00", 'EF44': "16:10"},
+    {'DS44':"14:10", 'PJ42':"16:10"}
        ]
 
 # preenchendo o dicionario 
@@ -21,9 +21,16 @@ semana['sexta'] = aulas[4]
 '''
 for dia,aula in semana.items():
     if len(aula) == 2: # se tiver duas aulas no dia
-        print(f'na {dia} as aulas são {aula[0]} e {aula[1]}')
-    else: # se tiver 3 aulas no dia 
+        print(f'na {dia} as aulas são {aula}')
+    else:
         print(f'na {dia} as aulas são {aula[0]} e {aula[1]} e {aula[2]}')
 '''
+
+'''
+resp = input('qual dia voce deseja consultar? ')
+
+if resp.lower() == 'segunda':
+'''
+
 
 
